79671cb341dc95bdcb06cbf2912bffdb0774bef7
Nessaa16 <120008781+Nessaa16@users.noreply.github.com>
2024-12-10 13:31:46 +0700

    fixing minor bugs and add change to character page
    fixing overlaps screen bugs and combining all character pages into 1

--- wordwhizz/lib/view/pages/character_page.dart
+++ wordwhizz/lib/view/pages/character_page.dart
@@ -8,11 +8,44 @@ class CharacterPage extends StatefulWidget {
 }
 
 class _CharacterPageState extends State<CharacterPage> {
-  String selectedCharacter = "";
+  final List<Map<String, String>> _characters = [
+    {
+      'name': 'Tikus Ksatria',
+      'image': 'assets/images/tikus.png',
+      'description': 'assets/images/deskripsiTikus.png',
+    },
+    {
+      'name': 'Kucing Penyihir',
+      'image': 'assets/images/kucing.png',
+      'description': 'assets/images/deskripsiKucing.png',
+    },
+    {
+      'name': 'Kerbau Pejuang',
+      'image': 'assets/images/kerbau.png',
+      'description': 'assets/images/deskripsiKerbau.png',
+    },
+    {
+      'name': 'Rubah Pemanah',
+      'image': 'assets/images/rubah.png',
+      'description': 'assets/images/deskripsiRubah.png',
+    },
+  ];
+
   int _currentIndex = 3;
+
+  void _changeCharacter(int direction) {
+    setState(() {
+      _currentIndex = (_currentIndex + direction) % _characters.length;
+      if (_currentIndex < 0) {
+        _currentIndex = _characters.length - 1;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    final currentCharacter = _characters[_currentIndex];
+
     return Scaffold(
       body: Stack(
         children: [
@@ -23,158 +56,166 @@ class _CharacterPageState extends State<CharacterPage> {
             ),
           ),
           SafeArea(
-            bottom: false,
-            child: ListView(
-              padding: EdgeInsets.all(20.0),
-              children: [
-                 Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: List.generate(4, (index) {
-                    return AnimatedContainer(
-                      duration: Duration(milliseconds: 300),
-                      margin: EdgeInsets.symmetric(horizontal: 5),
-                      height: 8,
-                      width: 8,
-                      decoration: BoxDecoration(
-                        color: _currentIndex == index
-                            ? Color.fromARGB(255, 50, 44, 97)
-                            : Colors.white,
-                        shape: BoxShape.circle,
-                      ),
-                    );
-                  }),
-                ),
-                SizedBox(height: 30),
-
-                // Logo
-                Center(
-                  child: Image.asset(
-                    'assets/images/logo.png',
-                    height: 200,
-                    width: 220,
-                  ),
-                ),
-
-                // Title
-                Text(
-                  "Pilih Karaktermu",
-                  style: TextStyle(
-                    fontFamily: 'BalooChettan2',
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(2.0, 2.0),
-                        blurRadius: 4.0,
-                        color: Colors.black54,
-                      ),
-                    ],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 10),
-                 // Character Name
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(30),
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.black26,
-                                blurRadius: 9.0,
-                                offset: Offset(4, 2),
-                              ),
-                            ],
-                          ),
-                          padding: EdgeInsets.all(20),
-                          child: Text(
-                            "Tikus Ksatria",
-                            style: character
-                          ),
-                        ),
-                      ],
-                    ),
-
-                // Character options
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          selectedCharacter = "Tikus Ksatria";
-                        });
-                      },
-                      child: Image.asset(
-                        'assets/images/tikus.png',
-                        height: 250,
-                        width: 250,
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Character2Page(
-                              selectedCharacter: selectedCharacter,
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                return SingleChildScrollView(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(minHeight: constraints.maxHeight),
+                    child: IntrinsicHeight(
+                      child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: List.generate(_characters.length, (index) {
+                                return AnimatedContainer(
+                                  duration: const Duration(milliseconds: 300),
+                                  margin: const EdgeInsets.symmetric(horizontal: 5),
+                                  height: 8,
+                                  width: 8,
+                                  decoration: BoxDecoration(
+                                    color: _currentIndex == index
+                                        ? const Color.fromARGB(255, 50, 44, 97)
+                                        : Colors.white,
+                                    shape: BoxShape.circle,
+                                  ),
+                                );
+                              }),
                             ),
-                          ),
-                        );
-                      },
-                      child: Image.asset(
-                        'assets/images/buttonRight.png',
-                        height: 50,
-                        width: 50,
+                            const SizedBox(height: 30),
+
+                            // Logo
+                            Image.asset(
+                              'assets/images/logo.png',
+                              height: constraints.maxHeight * 0.2,
+                              width: constraints.maxWidth * 0.6,
+                            ),
+
+                            // Title
+                            const Text(
+                              "Pilih Karaktermu",
+                              style: TextStyle(
+                                fontFamily: 'BalooChettan2',
+                                fontSize: 32.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                shadows: [
+                                  Shadow(
+                                    offset: Offset(2.0, 2.0),
+                                    blurRadius: 4.0,
+                                    color: Colors.black54,
+                                  ),
+                                ],
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                            const SizedBox(height: 10),
+
+                            // Character Name
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(30),
+                                boxShadow: const [
+                                  BoxShadow(
+                                    color: Colors.black26,
+                                    blurRadius: 9.0,
+                                    offset: Offset(4, 2),
+                                  ),
+                                ],
+                              ),
+                              padding: const EdgeInsets.all(20),
+                              child: Text(
+                                currentCharacter['name']!,
+                                style: character
+                              ),
+                            ),
+
+                            const SizedBox(height: 20),
+
+                            // Character options
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                GestureDetector(
+                                  onTap: () => _changeCharacter(-1),
+                                  child: Image.asset(
+                                    'assets/images/buttonLeft.png',
+                                    height: 50,
+                                    width: 50,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () {},
+                                  child: Image.asset(
+                                    currentCharacter['image']!,
+                                    height: constraints.maxHeight * 0.25,
+                                    width: constraints.maxWidth * 0.5,
+                                  ),
+                                ),
+                                GestureDetector(
+                                  onTap: () => _changeCharacter(1),
+                                  child: Image.asset(
+                                    'assets/images/buttonRight.png',
+                                    height: 50,
+                                    width: 50,
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                            const SizedBox(height: 20),
+
+                            // Character description
+                            Image.asset(
+                              currentCharacter['description']!,
+                              height: constraints.maxHeight * 0.18,
+                              width: constraints.maxWidth * 0.6,
+                            ),
+
+                            const Spacer(),
+
+                            // Choose Button
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ProfilePage(
+                                      selectedCharacter: currentCharacter['name']!,
+                                    ),
+                                  ),
+                                );
+                              },
+                              child: Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  Image.asset(
+                                    'assets/images/button1.png',
+                                    height: 60,
+                                    width: 250,
+                                  ),
+                                  const Positioned(
+                                    child: Text(
+                                      "Pilih",
+                                      style: TextStyle(
+                                        fontSize: 20,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ],
-                ),
-
-                Positioned(
-                  child: Image.asset(
-                    'assets/images/deskripsiTikus.png',
-                    height: 150,
-                    width: 150,
                   ),
-                ),
-                
-                Align(
-                  alignment: Alignment.center,
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => ProfilePage(
-                            selectedCharacter: selectedCharacter,
-                          ),
-                        ),
-                      );
-                    },
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Image.asset(
-                          'assets/images/button1.png',
-                          height: 60,
-                          width: 250, 
-                        ),
-                        Positioned(
-                          child: Text(
-                            "Pilih", 
-                            style: button1
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+                );
+              },
             ),
           ),
         ],
@@ -182,3 +223,4 @@ class _CharacterPageState extends State<CharacterPage> {
     );
   }
 }
+
--- wordwhizz/lib/view/pages/pages.dart
+++ wordwhizz/lib/view/pages/pages.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:wordwhizz/provider/auth_service.dart';
+import 'package:wordwhizz/services/auth_service.dart';
 import 'package:wordwhizz/shared/shared.dart';
 import 'package:wordwhizz/view/pages/splashscreen.dart';
 import 'package:wordwhizz/firebase_options.dart';
@@ -12,7 +12,4 @@ part 'login_page.dart';
 part 'profileSetUp_page.dart';
 part 'rules_page.dart';
 part 'character_page.dart';
-part 'character2_page.dart';
-part 'character3_page.dart';
-part 'character4_page.dart';
 part 'profile_page.dart';
