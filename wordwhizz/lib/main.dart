636e4d9991386b7b582054f9d4b85644c47c7af8
xyasth <pkosasih@student.ciputra.ac.id>
2024-12-13 15:34:05 +0700

    update Gacha UI

--- wordwhizz/lib/main.dart
+++ wordwhizz/lib/main.dart
@@ -18,9 +18,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GachaPage(),
+      home: WelcomePage(),
     );
   }
 }
--- wordwhizz/lib/view/pages/gacha_page.dart
+++ wordwhizz/lib/view/pages/gacha_page.dart
@@ -8,7 +8,6 @@ class GachaPage extends StatefulWidget {
 }
 
 class _GachaPageState extends State<GachaPage> {
-  // Rewards list
   final List<String> _rewards = [
     '⭐ gold',
     '⭐ gold',
@@ -22,100 +21,117 @@ class _GachaPageState extends State<GachaPage> {
     '⭐⭐⭐ Hint Potion'
   ];
 
-  String? _currentReward; // Currently drawn reward
-  bool _isAnimationPlaying = false; // To track if the animation is running
+  String? _currentReward;
+  bool _isAnimationPlaying = false;
 
   void _startGacha() {
     setState(() {
-      _isAnimationPlaying = true; // Start animation
+      _isAnimationPlaying = true;
+      _currentReward = null;
     });
 
-    // Simulate animation duration (e.g., 3 seconds)
     Future.delayed(const Duration(seconds: 3), () {
       final random = Random();
       final randomIndex = random.nextInt(_rewards.length);
 
       setState(() {
         _currentReward = _rewards[randomIndex];
-        _isAnimationPlaying = false; // Stop animation
+        _isAnimationPlaying = false;
       });
+    });
+  }
+
+  void _hideRewardScreen() {
+    setState(() {
+      _currentReward = null;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          // Background image
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage(
-                    'assets/images/bgsplash.png'), // Your background image
-                fit: BoxFit.cover,
+      body: GestureDetector(
+        onTap: _isAnimationPlaying || _currentReward != null
+            ? _hideRewardScreen
+            : null,
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/bgsplash.png'),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-
-          // Main content
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Reward Display (after animation)
-                if (_currentReward != null && !_isAnimationPlaying)
-                  Column(
-                    children: [
-                      Text(
-                        '🎉 Congratulations! 🎉',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.orange[800],
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (_isAnimationPlaying ||
+                      (_currentReward != null && !_isAnimationPlaying))
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          if (_isAnimationPlaying)
+                            Lottie.asset(
+                              'assets/images/animations/gacha.json',
+                              width: 200,
+                              height: 200,
+                            ),
+                          if (!_isAnimationPlaying && _currentReward != null)
+                            Column(
+                              children: [
+                                Text(
+                                  '🎉 Congratulations! 🎉',
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.orange[800],
+                                  ),
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  _currentReward!,
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                    color: Colors.black87,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          const SizedBox(height: 30),
+                        ],
+                      ),
+                    ),
+                  if (!_isAnimationPlaying && _currentReward == null)
+                    ElevatedButton(
+                      onPressed: _startGacha,
+                      style: ElevatedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 30, vertical: 15),
+                        backgroundColor: Colors.orange,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
                       ),
-                      const SizedBox(height: 10),
-                      Text(
-                        _currentReward!,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                            fontSize: 20, color: Colors.black87),
-                      ),
-                    ],
-                  ),
-
-                // Animation Display (during gacha pull)
-                if (_isAnimationPlaying)
-                  Lottie.asset(
-                    'assets/images/animations/gacha.json', // Your chest animation JSON file
-                    width: 200,
-                    height: 200,
-                  ),
-
-                const SizedBox(height: 30),
-
-                // Gacha Button
-                if (!_isAnimationPlaying)
-                  ElevatedButton(
-                    onPressed: _startGacha,
-                    style: ElevatedButton.styleFrom(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 30, vertical: 15),
-                      backgroundColor: Colors.orange,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
+                      child: const Text(
+                        'Gacha',
+                        style: TextStyle(fontSize: 20, color: Colors.white),
                       ),
                     ),
-                    child: const Text(
-                      'Gacha',
-                      style: TextStyle(fontSize: 20, color: Colors.white),
-                    ),
-                  ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
