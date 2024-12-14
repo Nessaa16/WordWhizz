493e18ef7da672850725b439ac82bec037b5a43b
Marshanda <marshanda01@student.ciputra.ac.id>
2024-12-14 17:34:55 +0700

    fixing gacha button

--- wordwhizz/lib/view/pages/shop_page.dart
+++ wordwhizz/lib/view/pages/shop_page.dart
@@ -10,145 +10,170 @@ class ShopPage extends StatelessWidget {
         children: [
           // Background Image
           Image.asset(
-            '/Users/marshandagwie/development/projects_flutter/WordWhizz/wordwhizz/assets/images/bgShopPage.png',
+            'assets/images/bgShopPage.png',
             fit: BoxFit.cover,
             width: double.infinity,
             height: double.infinity,
           ),
           SafeArea(
-            child: Column(
-              children: [
-                // Header
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Row(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.start, // Ensure proper alignment
+                children: [
+                  // Header
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {
+                            // Handle back navigation
+                          },
+                          child: Image.asset(
+                            'assets/images/backarrow.png',
+                            width: 40,
+                            height: 40,
+                            fit: BoxFit.contain,
+                          ),
                         ),
-                        child: Icon(Icons.arrow_back_ios, color: Colors.orange),
-                      ),
-                      Spacer(),
-                      // Coins
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12),
+                        Spacer(),
+                        // Coins
+                        Container(
+                          width: 80,
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Row(
+                            children: [
+                              Image.asset('assets/images/coin.png', height: 24),
+                              SizedBox(width: 4),
+                              Text('350',
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                            ],
+                          ),
                         ),
-                        child: Row(
-                          children: [
-                            Image.asset('/Users/marshandagwie/development/projects_flutter/WordWhizz/wordwhizz/assets/images/coin.png', height: 24),
-                            SizedBox(width: 4),
-                            Text('350', style: TextStyle(fontWeight: FontWeight.bold)),
+                        SizedBox(width: 16),
+                        // Hearts
+                        Container(
+                          width: 80,
+                          padding:
+                              EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Row(
+                            children: [
+                              Image.asset('assets/images/heart.png',
+                                  height: 24),
+                              SizedBox(width: 4),
+                              Text('8',
+                                  style:
+                                      TextStyle(fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  // Title
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: Center(
+                      child: Text(
+                        'Toko & Gacha',
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          shadows: [
+                            Shadow(
+                              offset: Offset(2, 2),
+                              blurRadius: 3.0,
+                              color: Colors.black.withOpacity(0.3),
+                            ),
                           ],
                         ),
                       ),
-                      SizedBox(width: 8),
-                      // Hearts
-                      Container(
-                        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        child: Row(
-                          children: [
-                            Icon(Icons.favorite, color: Colors.red),
-                            SizedBox(width: 4),
-                            Text('8', style: TextStyle(fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
-                ),
-                
-                // Title
-                Text(
-                  'Toko & Gacha',
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    shadows: [
-                      Shadow(
-                        offset: Offset(2, 2),
-                        blurRadius: 3.0,
-                        color: Colors.black.withOpacity(0.3),
-                      ),
-                    ],
-                  ),
-                ),
-                
-                Expanded(
-                  child: SingleChildScrollView(
-                    padding: EdgeInsets.all(16),
+
+                  // Sections
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
                     child: Column(
                       children: [
-                        // Koin Section
                         _buildSection(
                           'Koin',
                           [
-                            _buildItem('200', 'IDR 25K', '/Users/marshandagwie/development/projects_flutter/WordWhizz/wordwhizz/assets/images/coins_200.png'),
-                            _buildItem('400', 'IDR 50K', '/Users/marshandagwie/development/projects_flutter/WordWhizz/wordwhizz/assets/images/coins_400.png'),
-                            _buildItem('600', 'IDR 75K', '/Users/marshandagwie/development/projects_flutter/WordWhizz/wordwhizz/assets/images/coins_600.png'),
+                            _buildItem('200', 'IDR 25K',
+                                'assets/images/coins_200.png'),
+                            _buildItem('400', 'IDR 50K',
+                                'assets/images/coins_400.png'),
+                            _buildItem('600', 'IDR 75K',
+                                'assets/images/coins_600.png'),
                           ],
                         ),
-                        
                         SizedBox(height: 16),
-                        
-                        // Nyawa Section
                         _buildSection(
                           'Nyawa',
                           [
-                            _buildItem('Full', '100 koin', '/Users/marshandagwie/development/projects_flutter/WordWhizz/wordwhizz/assets/images/heart.png'),
-                            _buildItem('4', '50 koin', '/Users/marshandagwie/development/projects_flutter/WordWhizz/wordwhizz/assets/images/hearts_4.png'),
-                            _buildItem('2', '25 koin', '/Users/marshandagwie/development/projects_flutter/WordWhizz/wordwhizz/assets/images/hearts_2.png'),
+                            _buildItem(
+                                'Full', '100 koin', 'assets/images/heart.png'),
+                            _buildItem(
+                                '4', '50 koin', 'assets/images/hearts_4.png'),
+                            _buildItem(
+                                '2', '25 koin', 'assets/images/hearts_2.png'),
                           ],
                         ),
-                        
                         SizedBox(height: 16),
-                        
-                        // Paket Section
                         _buildSection(
                           'Paket',
                           [
-                            _buildItem('Pemula', '50 koin', '/Users/marshandagwie/development/projects_flutter/WordWhizz/wordwhizz/assets/images/bundlePemula.png'),
-                            _buildItem('Elit', '100 koin', '/Users/marshandagwie/development/projects_flutter/WordWhizz/wordwhizz/assets/images/bundleElit.png'),
-                            _buildItem('Sultan', '200 koin', '/Users/marshandagwie/development/projects_flutter/WordWhizz/wordwhizz/assets/images/bundleSultan.png'),
+                            _buildItem('Pemula', '50 koin',
+                                'assets/images/bundlePemula.png'),
+                            _buildItem('Elit', '100 koin',
+                                'assets/images/bundleElit.png'),
+                            _buildItem('Sultan', '200 koin',
+                                'assets/images/bundleSultan.png'),
                           ],
                         ),
                       ],
                     ),
                   ),
-                ),
-                
-                // Gacha Button
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Gacha',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.orange,
-                      minimumSize: Size(double.infinity, 50),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25),
+
+                  // Gacha Button
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Align(
+                      alignment: Alignment.center,
+                      child: GestureDetector(
+                        onTap: () {
+                          // Fungsi saat tombol ditekan
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(25),
+                            image: DecorationImage(
+                              image:
+                                  AssetImage('assets/images/button_gacha.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          width: 166.33,
+                          height: 58.17,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ],
@@ -161,7 +186,7 @@ class ShopPage extends StatelessWidget {
       padding: EdgeInsets.all(20),
       decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('/Users/marshandagwie/development/projects_flutter/WordWhizz/wordwhizz/assets/images/cardShopPage.png'),
+          image: AssetImage('assets/images/cardShopPage.png'),
           fit: BoxFit.cover,
         ),
         borderRadius: BorderRadius.circular(20),
@@ -222,4 +247,3 @@ class ShopPage extends StatelessWidget {
     );
   }
 }
-
