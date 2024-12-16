b6171222fd26399c9022c1f06af7907b6985c678
Marshanda <marshanda01@student.ciputra.ac.id>
2024-12-17 00:49:26 +0700

    ProfilePage UI 95%

--- wordwhizz/lib/view/pages/profile_page.dart
+++ wordwhizz/lib/view/pages/profile_page.dart
@@ -19,10 +19,10 @@ class _ProfilePageState extends State<ProfilePage> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Teks "Profile" di atas
+                
                 Stack(
                   children: [
-                    // Tombol Back menggunakan GestureDetector dan gambar
+                    
                     Positioned(
                       top: 10,
                       left: 10,
@@ -32,14 +32,14 @@ class _ProfilePageState extends State<ProfilePage> {
                               context); // Navigasi ke halaman sebelumnya
                         },
                         child: Image.asset(
-                          'assets/images/backarrow.png', // Path gambar
-                          width: 40, // Lebar gambar
-                          height: 40, // Tinggi gambar
-                          fit: BoxFit.contain, // Mengatur gambar sesuai ukuran
+                          'assets/images/backarrow.png', 
+                          width: 40, 
+                          height: 40, 
+                          fit: BoxFit.contain, 
                         ),
                       ),
                     ),
-                    // Teks "Profile" di tengah
+                    
                     Center(
                       child: Text(
                         'Profile',
@@ -72,7 +72,7 @@ class _ProfilePageState extends State<ProfilePage> {
                         shape: BoxShape.circle,
                       ),
                       child: Image.asset(
-                        'assets/images/avatar_dog.png', // Ganti dengan path gambar Anda
+                        'assets/images/avatar_dog.png', 
                       ),
                     ),
                   ],
@@ -81,73 +81,143 @@ class _ProfilePageState extends State<ProfilePage> {
 
                 // Profile Info
                 Center(
-                  child: Column(
-                    children: [
-                      Text(
-                        'Depede Swangar',
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.brown[700],
+                  child: Container(
+                    padding: const EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius:
+                          BorderRadius.circular(40), 
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 5,
+                          offset: const Offset(0, 2),
                         ),
-                      ),
-                      const Text(
-                        '14 years old',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Colors.brown,
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        // Profile Info
+                        Column(
+                          children: [
+                            Text(
+                              'Depede Swangar',
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.brown[700],
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment
+                                  .center, 
+                              children: [
+                                const Text(
+                                  '14 years old',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.brown,
+                                  ),
+                                ),
+                                const SizedBox(
+                                    width: 8), 
+                                GestureDetector(
+                                  onTap: () {
+                                    // Tambahkan fungsi edit 
+                                    print('Edit icon pressed');
+                                  },
+                                  child: const Icon(
+                                    Icons.edit, // Ikon edit
+                                    size: 20, 
+                                    color: Colors.brown, 
+                                  ),
+                                ),
+                              ],
+                            ),
+
+                            // Garis Pembatas
+                            const SizedBox(height: 8),
+                            Center(
+                              child: Container(
+                                width: 234,
+                                height: 1,
+                                color: Color(
+                                    0xFFD3D2D2), 
+                              ),
+                            ),
+
+                            const SizedBox(height: 8),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment
+                                  .center, 
+                              children: [
+                                _buildStat('Level', '15'),
+                                const SizedBox(
+                                    width:
+                                        24), 
+                                _buildStat('Exp', '1221/2000'),
+                                const SizedBox(
+                                    width:
+                                        24), 
+                                _buildStat('Chapter', '2'),
+                              ],
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 20),
 
-                // Stats Row
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      _buildStat('Level', '15'),
-                      _buildStat('Exp', '1221/2000'),
-                      _buildStat('Chapter', '2'),
-                    ],
-                  ),
-                ),
-                const SizedBox(height: 30),
+                        const SizedBox(height: 8),
 
-                // Achievements Section
-                const Text(
-                  'Achievement',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.brown,
-                  ),
-                ),
-                const SizedBox(height: 20),
+                        // Garis Pembatas 
+                        Center(
+                          child: Container(
+                            width: 234,
+                            height: 1,
+                            color: Color(
+                                0xFFD3D2D2), 
+                          ),
+                        ),
 
-                // Achievement Cards
-                _buildAchievementCard(
-                  'Genius People',
-                  'You have completed this lesson 10 times.',
-                  Colors.purple.shade200,
-                  Icons.emoji_events,
-                ),
-                const SizedBox(height: 10),
-                _buildAchievementCard(
-                  'No Buff-Buff Club',
-                  'You have completed this lesson 10 times without any buff.',
-                  Colors.lightGreen,
-                  Icons.person,
-                ),
-                const SizedBox(height: 10),
-                _buildAchievementCard(
-                  'Speedrunner',
-                  'You have completed this lesson 10 times without any buff.',
-                  Colors.amber,
-                  Icons.timer,
+                        const SizedBox(height: 8),
+
+                        // Achievements Section
+                        const Center(
+                          child: Text(
+                            'Achievement',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.brown,
+                            ),
+                          ),
+                        ),
+
+                        const SizedBox(height: 20),
+
+                        // Achievement Cards
+                        _buildAchievementCard(
+                          'Genius People',
+                          'You have completed this lesson 10 times.',
+                          const Color(0xFFAD79EB),
+                          'assets/images/achieve_genius.png', 
+                        ),
+                        const SizedBox(height: 10),
+                        _buildAchievementCard(
+                          'No Buff-Buff Club',
+                          'You have completed this lesson 10 times without any buff.',
+                          const Color(0xFFA7EC3A),
+                          'assets/images/achieve_nobuff.png', 
+                        ),
+                        const SizedBox(height: 10),
+                        _buildAchievementCard(
+                          'Speedrunner',
+                          'You have completed this lesson 10 times without any buff.',
+                          const Color(0xFFFFDE00),
+                          'assets/images/achieve_speedrunner.png', 
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -164,6 +234,7 @@ class _ProfilePageState extends State<ProfilePage> {
           label,
           style: const TextStyle(
             fontSize: 16,
+            fontWeight: FontWeight.bold,
             color: Colors.brown,
           ),
         ),
@@ -171,7 +242,6 @@ class _ProfilePageState extends State<ProfilePage> {
           value,
           style: const TextStyle(
             fontSize: 18,
-            fontWeight: FontWeight.bold,
             color: Colors.brown,
           ),
         ),
@@ -180,13 +250,13 @@ class _ProfilePageState extends State<ProfilePage> {
   }
 
   Widget _buildAchievementCard(
-      String title, String description, Color color, IconData icon) {
+      String title, String description, Color color, String imagePath) {
     return Container(
       width: double.infinity,
       padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
         color: color,
-        borderRadius: BorderRadius.circular(20),
+        borderRadius: BorderRadius.circular(45),
         boxShadow: [
           BoxShadow(
             color: Colors.black.withOpacity(0.1),
@@ -199,12 +269,13 @@ class _ProfilePageState extends State<ProfilePage> {
       child: Row(
         children: [
           Container(
-            padding: const EdgeInsets.all(8),
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              shape: BoxShape.circle,
+            padding: const EdgeInsets.all(1),
+            child: Image.asset(
+              imagePath, 
+              width: 66, 
+              height: 66, 
+              fit: BoxFit.cover,
             ),
-            child: Icon(icon, color: color),
           ),
           const SizedBox(width: 16),
           Expanded(
